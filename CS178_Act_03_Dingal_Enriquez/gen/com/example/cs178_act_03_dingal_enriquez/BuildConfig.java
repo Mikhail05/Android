9da7d2c0b65ffe6ce04c36cd168e6d66699edb47
Michael Dingal <mike_thegriever@yahoo.com>
2013-03-14 21:57:39 +0800

    hello
    hi

--- CS178_Act_03_Dingal_Enriquez/gen/com/example/cs178_act_03_dingal_enriquez/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.cs178_act_03_dingal_enriquez;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
